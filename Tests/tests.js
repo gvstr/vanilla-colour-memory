22a6d380f98ec5a54d50c94e6110dcd8dc6f573d
G S <gustav.strid@gmail.com>
2021-06-27 23:15:51 +0200

    more tests

--- Tests/tests.js
+++ Tests/tests.js
@@ -3,6 +3,17 @@ const expect = chai.expect;
 const match = chai.match;
 const assert = chai.assert;
 
+describe('shuffleArray(array)', () => {
+    it('should rearrange the array', () => {
+        let originalArray = [0,1,2,3,4,5,6,7,8,9];
+        let copy = originalArray.slice();
+        expect(originalArray).to.eql(copy)
+        shuffleArray(copy);
+        expect(originalArray).to.have.members(copy)
+        expect(originalArray).to.not.eql(copy)
+    })
+});
+
 describe('getRandomColourString()', () => {
     it('Should return a string', () => {
         assert.typeOf(getRandomColourString(), 'string')
@@ -16,17 +27,9 @@ describe('getRandomColourString()', () => {
 describe('delay(ms)', () => {
     let delayTimeInMs = 2000;
     let allowedTime = delayTimeInMs * 1.05;
-    it('Should not resolve until timeout', async () => {
+    it('Should not resolve until after timeout', async () => {
         await delay(delayTimeInMs);
         expect(true).to.be.true;
     }).timeout(allowedTime); //default mocha timeout = 2000
 });
 
-describe('shuffleArray(array)', () => {
-    let delayTimeInMs = 2000;
-    let allowedTime = delayTimeInMs * 1.05;
-    it('Should not resolve until timeout', async () => {
-        await delay(delayTimeInMs);
-        expect(true).to.be.true;
-    }).timeout(allowedTime); //default mocha timeout = 2000
-});
